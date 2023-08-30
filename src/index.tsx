fc44f0824dc4d35507b995e295cf6ba801d5ce72
_ <bdowling@ucsc.edu>
2023-08-29 23:59:48 -0700

    movement

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const SCREEN_HEIGHT = window.innerHeight;
+const SCREEN_WIDTH = window.innerWidth;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BOX_WIDTH = 100;
+
+
+function sleep(ms:number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+export default function Index() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [xV, setXV] = useState(0);
+  const [yV, setYV] = useState(10);
+  const GRAV = 1;
+  const stylesheet = {
+    position: 'relative',
+    top: y,
+    left: x,
+  } as any;
+
+
+  function move(){
+    setX(prevX => {return prevX+xV});
+    console.log(yV)
+    setY(prevY => {
+      if(SCREEN_HEIGHT-BOX_WIDTH <= prevY+yV){
+        setYV(prevYV => {return -Math.abs(prevYV)})
+      }
+      return prevY+yV;
+    });
+  }
+  function accel(){
+    setYV(prevYV => {console.log(prevYV);return prevYV+GRAV});
+
+  }
+  function loop(){
+    accel();
+      
+    move();
+  }
+  useEffect(() => {
+    const interval = setInterval(() =>{ move();accel();}, 10);
+    return () => clearInterval(interval);
+  }, [yV]);
+
+
+  return (
+    <div className="container">
+      <div className="shape" style={stylesheet}>
+
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<Index/>, document.getElementById('root'))
